a25528c1392df6345dfd021b01c515eb4545837a
hridyansh5492 <hridyanshsati4321@gmail.com>
2026-04-06 09:16:12 +0530

    moved contact form logic to new js file

--- js/index.js
+++ js/index.js
@@ -4,11 +4,6 @@ let index = 0;
 let charIndex = 0;
 let isDeleting = false;
 
-//for contact form
-const form = document.getElementById("contform");
-const overlay = document.getElementById("overlay");
-const overlayMsg = document.getElementById("overlay-message");
-
 function typeLoop() {
     const currentText = texts[index];
     
@@ -48,36 +43,3 @@ window.onload = function() {
         }
     });
 };
-
-//contact form submit request
-form.addEventListener("submit", function (e) {
-e.preventDefault(); // Prevent default form submission
-
-const formData = new FormData(form);
-
-fetch(form.action, {
-    method: "POST",
-    body: formData,
-    headers: {
-    'Accept': 'application/json'
-    }
-}).then(response => {
-    if (response.ok) {
-    showOverlay("✅ Message Sent Successfully!");
-    form.reset(); // Clear form
-    } else {
-    showOverlay("❌ Failed to Send Message.");
-    }
-}).catch(() => {
-    showOverlay("❌ Failed to Send Message.");
-});
-});
-
-function showOverlay(message) {
-overlayMsg.textContent = message;
-overlay.style.display = "flex";
-
-setTimeout(() => {
-    overlay.style.display = "none";
-}, 3000); // Hide after 3 seconds
-}
